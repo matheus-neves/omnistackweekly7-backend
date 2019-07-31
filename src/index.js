7e38813dd33eaa890ab8b221f399bfdecaaa1069
Matheus Neves <matheus.neves@prodigious.com>
2019-07-31 10:23:01 -0300

    update package.json / fix script start

--- src/index.js
+++ src/index.js
@@ -18,7 +18,8 @@ app.use((req, res, next) => {
 });
 
 app.use(cors());
-
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use('/files', express.static(path.resolve(__dirname, '..', 'uploads', 'resized')))
 
 app.use(require('./routes'));
